efd9bff521fe6d896efd25cc306296d84660a0eb
Daniil Mikhailov <danil@etersoft.ru>
2014-05-25 22:33:49 +0400

    First realisation pressure in drop calculation [not tested]

--- src/Drop.java
+++ src/Drop.java
@@ -32,42 +32,93 @@ public class Drop {
         }
     }
 
+    double[][] pressure;
+    private double nextPressureIteration() {
+        pressure = getInitialPressure();
+        double[][] newPressure = new double[X][Y];
+        double diff = 0;
+        double deltaTime = 0.01; //dT
+        double deltaR = 0.1; //dX
+        double deltaY = 0.1; //dX
+        double height = getHeightDrop();
+
+        double alpha = 0.1;  // ( m * lambda ) / ( r * p )
+                            // вязкость * тепл проводность / ( уд тепл парообраз * плотность пара )
+        double delta = deltaTime * alpha / ( deltaR * height * height * height * height); // TODO check deltaR
+        double t0 = 100;
+
+        for (int i = 1; i < dropHeat.length; i++) {
+            for (int j = 1; j < dropHeat[1].length; j++) {
+                newPressure[i][j] = pressure[i][j] - delta * dropHeat[i][j]; //dropHeat[i][j] - t0
+                diff += newPressure[i][j] - pressure[i][j];
+            }
+        }
+
+        pressure = newPressure;
+        return diff;
+    }
+
+
+
+    private double[][] getInitialPressure() {
+        pressure = new double[dropSize][dropSize];
+        double atmospherePressure = 600;
+
+        for (int i = 0; i < pressure.length; i++) {
+            double[] doubles = pressure[i];
+            for (int i1 = 0; i1 < doubles.length; i1++) {
+                doubles[i1] = atmospherePressure;
+            }
+        }
+
+        return pressure;
+    }
+
+    private double getHeightDrop() {
+        return 0;
+    }
+
     public void nextStep(){
         setDropHeat(X, Y); //how many heat go to water
         getDirection();
     }
 
-//    private void setDropHeat(int x, int y) {
-//        setDropHeat(x, y);
-//    }
+    private void getDirection() {
+        int[] dir = getBorder(dropHeat);
+        X = dir[0];
+        Y = dir[1];
+    }
+    private int[] getBorder(double[][] array) {
 
-    private void getDirection() { //TODO drop size must be > 2 and doesn't stay at border
-
-        if (dropSize < 2 || dropHeat.length < 2 || X == 0 || Y == 0 ||
+        if (dropSize < 2 || array.length < 2 || X == 0 || Y == 0 ||
                 X == heat.length - 1 || Y == heat[1].length - 1){
             System.out.println("drop size must be > 2 and doesn't stay at border " + dropSize + " " + X );
         }
 
-        int dropHeatXLength = dropHeat.length;
-        int dropHeatYLength = dropHeat[1].length;
+        int dropHeatXLength = array.length;
+        int dropHeatYLength = array[1].length;
 
-        double [] up = new double[dropHeatXLength];
-        double [] down = new double[dropHeatXLength];
+        double[] up = new double[dropHeatXLength];
+        double[] down = new double[dropHeatXLength];
         double[] left = new double[dropHeatYLength];
         double[] right = new double[dropHeatYLength];
 
         for (int i = 0; i < dropHeatXLength; i++) {
-            up[i] = dropHeat[i][1] - dropHeat[i][0];
-            down[i] = dropHeat[i][dropHeatXLength - 2] - dropHeat[i][dropHeatXLength - 1];
+            up[i] = array[i][1] - array[i][0];
+            down[i] = array[i][dropHeatXLength - 2] - array[i][dropHeatXLength - 1];
         }
 
         for (int j = 0; j < dropHeatYLength; j++) {
-            left[j] = dropHeat[1][j] - dropHeat[0][j];
-            right[j] = dropHeat[dropHeatYLength - 2][j] - dropHeat[dropHeatYLength - 1][j];
+            left[j] = array[1][j] - array[0][j];
+            right[j] = array[dropHeatYLength - 2][j] - array[dropHeatYLength - 1][j];
         }
 
-        X = getDir(X, up, down);
-        Y = getDir(Y, left, right);
+        //TODO return 4 arrays and rewrite get dir
+
+        int upDown = getDir(X, up, down);
+        int leftRight = getDir(Y, left, right);
+
+        return new int[] {upDown, leftRight};
     }
 
     private int getDir(int var, double [] first, double [] second){
